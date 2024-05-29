5c036d150b4bf7027079bceee2bf83fc00276c70
Nomaan <nomaan07.dev@gmail.com>
2024-05-30 00:18:38 +0330

    Refactor: mobileMenuToggle func

--- js/app.js
+++ js/app.js
@@ -1,9 +1,8 @@
 const mobileMenuToggleBtn = document.querySelector(".mobile-menu__toggle-btn");
 const barsElements = document.querySelectorAll(".bar");
 
-function mobileMenuToggle() {
-  this.classList.toggle("bars-3--open");
+const mobileMenuToggle = () => {
   barsElements.forEach((bar) => bar.classList.toggle("mobile-menu__toggle-btn--open"));
-}
+};
 
 mobileMenuToggleBtn.addEventListener("click", mobileMenuToggle);
