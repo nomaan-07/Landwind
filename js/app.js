80819ceacbf8794ce6d018ed36b57a937151723d
Nomaan <nomaan07.dev@gmail.com>
2024-05-30 04:16:09 +0330

    Feat: toggle mobile menu

--- js/app.js
+++ js/app.js
@@ -1,8 +1,15 @@
 const mobileMenuToggleBtn = document.querySelector(".mobile-menu__toggle-btn");
 const barsElements = document.querySelectorAll(".bar");
+const overlayElem = document.querySelector(".overlay");
+const mobileMenuElem = document.querySelector(".mobile-menu");
 
+const visibilityClasses = ["opacity-0", "invisible", "opacity-1", "visible"];
 const mobileMenuToggle = () => {
   barsElements.forEach((bar) => bar.classList.toggle("mobile-menu__toggle-btn--open"));
+  visibilityClasses.forEach((item) => overlayElem.classList.toggle(item));
+  mobileMenuElem.classList.toggle("-translate-y-[380px]");
+  mobileMenuElem.classList.toggle("translate-y-0");
 };
 
 mobileMenuToggleBtn.addEventListener("click", mobileMenuToggle);
+overlayElem.addEventListener("click", mobileMenuToggle);
