09edeeb24dfbda761e568a4e56505cdb989e7d97
Nomaan <nomaan07.dev@gmail.com>
2024-05-31 01:36:55 +0330

    add tailwind.config.js and input.css files

---
+++ tailwind.config.js
@@ -0,0 +1,23 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./**/*.{html,js}"],
+  theme: {
+    extend: {
+      fontFamily: {
+        Inter: "Inter",
+        InterMedium: "Inter Medium",
+        InterSemiBold: "Inter SemiBold",
+        InterExtraBold: "Inter ExtraBold",
+        InterBlack: "Inter Black",
+      },
+      container: {
+        center: true,
+        padding: {
+          DEFAULT: "0.625rem",
+          xl: "0",
+        },
+      },
+    },
+  },
+  plugins: [],
+};
