2365e7958f976e5d4229ab50a49e58ffa1de1e6c
Nomaan <nomaan07.dev@gmail.com>
2024-06-01 02:26:01 +0330

    Feat: Responsive Hero Section

--- tailwind.config.js
+++ tailwind.config.js
@@ -12,16 +12,27 @@ module.exports = {
       },
       container: {
         center: true,
+        screens: {
+          xs: "100%",
+          sm: "640px",
+          md: "768px",
+          lg: "1024px",
+          xl: "1280px",
+          "2xl": "1536px",
+        },
         padding: {
           DEFAULT: "0.625rem",
           xl: "0",
         },
       },
+      screens: {
+        xs: "400px",
+      },
     },
   },
   plugins: [
     function ({ addVariant }) {
-      addVariant('child', '& > *');
+      addVariant("child", "& > *");
       addVariant("child-hover", "& > *:hover");
     },
   ],
